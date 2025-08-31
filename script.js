0508bbdc68ae684cdc5b4648ba0955bee9f7859b
Awais <mawais09901@gmail.com>
2025-08-31 18:31:55 +0500

    Tic Tac Toe Project Completed. Every thing is working fine.

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const CreatePlayer = (name, marker) => {
 const GameController = (function (doc) {
 
     const board = GameBoard.getBoard();
+    const dialog = doc.getElementById("winning-dialog");
+    const closeDialog = doc.getElementById("dialog-button");
+    const startButton = doc.querySelector(".start-button");
 
     const winningCombos = [
         [0, 1, 2],
@@ -31,6 +34,17 @@ const GameController = (function (doc) {
     const secondPlayer = CreatePlayer("WesBos", "O");
     let currentPlayer = firstPlayer;
     let gameOver = false;
+    
+    startButton.addEventListener("click",(event) => {
+        event.preventDefault();
+
+        firstPlayer.name = doc.getElementById("first-player").value;
+        secondPlayer.name = doc.getElementById("second-player").value;
+
+        doc.querySelector(".form-container").style.display = "none";
+        doc.querySelector(".game-container").style.display = "flex";
+        doc.querySelector(".buttons-container").style.display = "flex";
+    });
 
     const playRound = (marker, index) => {
 
@@ -48,16 +62,17 @@ const GameController = (function (doc) {
                 console.log("Place is already Takened.");
             }
 
-        }
+            if (CheckWinner(marker)) {
+                dialog.querySelector("p").textContent = `${currentPlayer.name} Marker:${currentPlayer.marker} is Winner!`
+                dialog.showModal();
+                gameOver = true;
+            }
 
-        if (CheckWinner(marker)) {
-            console.log(`${currentPlayer.name} Marker:${currentPlayer.marker} is Winner!`);
-            gameOver = true;
-        }
-
-        else if (checkTies()) {
-            console.log("Tied Up!");
-            gameOver = true;
+            else if (checkTies()) {
+                dialog.querySelector("p").textContent = "Tied Up!";
+                dialog.showModal();
+                gameOver = true;
+            }
         }
 
         currentPlayer = currentPlayer === firstPlayer ? secondPlayer : firstPlayer;
@@ -74,48 +89,89 @@ const GameController = (function (doc) {
         return board.every(index => index !== "");
     }
 
+    function restartGame() {
+        gameOver = false;
+        currentPlayer = firstPlayer;
+    }
+
+    closeDialog.addEventListener("click", () => {
+        dialog.close();
+    });
+
+
     const getCurrentPlayer = () => currentPlayer;
-    
+
     const getGameOver = () => gameOver;
 
-    return { playRound, getCurrentPlayer, getGameOver }
+    return { playRound, getCurrentPlayer, getGameOver, restartGame }
 
 })(document);
 
 const DisplayController = (function (doc) {
 
     const gameContainer = doc.querySelector(".game-container");
+    const restart = doc.querySelector(".restart-button");
+
     const board = GameBoard.getBoard();
     let id = 0;
-    console.log(board)
 
     const renderCells = () => {
         board.forEach(el => {
             const div = doc.createElement("div");
             div.classList.add("cell");
-            div.setAttribute("id",id)
+            div.setAttribute("data-id", id)
             div.textContent = el;
             gameContainer.appendChild(div);
-            
-            div.addEventListener("click",(event) => {
+
+            div.addEventListener("click", (event) => {
                 let currentPlayer = GameController.getCurrentPlayer();
                 let playGame = GameController.playRound;
                 let gameOver = GameController.getGameOver();
-                let target = event.target.getAttribute("id");
+                let target = event.target.getAttribute("data-id");
 
-                if (div.textContent === "" && !gameOver){
+                if (div.textContent === "" && !gameOver) {
                     div.textContent = currentPlayer.marker;
-                    playGame(currentPlayer.marker,target);
+                    playGame(currentPlayer.marker, target);
                 }
-                console.log(board)
             })
-            id+=1;
+            id += 1;
         })
     }
 
-    return{renderCells}
+    restart.addEventListener("click", () => {
+
+        // this code is also for filling every index "";
+        // for(let i = 0; i < board.length; i++){
+        //     board[i] = "";
+        // }
+
+        board.fill("");
+
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(cell => cell.textContent = "");
+
+        GameController.restartGame();
+    });
+
+    doc.querySelector(".reset-button").addEventListener("click",() => {
+
+        board.fill("");
+
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(cell => cell.textContent = "");
+
+        GameController.restartGame();
+
+        doc.querySelector(".game-container").style.display = "none";
+        doc.querySelector(".buttons-container").style.display = "none";
+        doc.querySelector(".form-container").style.display = "block";
+
+    });
+
+
+
+    return { renderCells }
 
 })(document);
 
 DisplayController.renderCells();
-
